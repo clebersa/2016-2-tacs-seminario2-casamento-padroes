0ee2fe018b8e752a75c3d2f60d4b773e2b70cbef
Cleber de Souza Alcântara <cleber.93cd@gmail.com>
2016-10-09 18:22:27 -0300

    Removendo condicional causador de erro do algoritmo e adicionando comentários.

--- CasamentoPadroes/src/main/java/br/ufg/inf/es/tacs/casamentoPadroes/kmp/AlgoritmoKMP.java
+++ CasamentoPadroes/src/main/java/br/ufg/inf/es/tacs/casamentoPadroes/kmp/AlgoritmoKMP.java
@@ -3,6 +3,7 @@ package br.ufg.inf.es.tacs.casamentoPadroes.kmp;
 import java.util.Arrays;
 
 /**
+ * Implementação do Algoritmo KMP (Knuth-Morris-Pratt) para casamento de padrão.
  *
  * @author cleber
  */
@@ -18,21 +19,27 @@ public class AlgoritmoKMP {
      * caso o padrão não ocorra no texto.
      */
     public static int indiceDe(String padrao, String texto) {
-        int[] arrayFalha = gerarArrayDeFalha(padrao);
-        int indiceTexto = 0;
-        int indicePadrao = 0;
-        while (indiceTexto < texto.length()) {
-            if (texto.charAt(indiceTexto) == padrao.charAt(indicePadrao)) {
-                if (indicePadrao == padrao.length() - 1) {
-                    return indiceTexto - indicePadrao;
+        // Esta verificação condicional não está originalmente no algoritmo, mas 
+        // foi inserida aqui para tratar os casos onde o padrão é nulo ou vazio.
+        if (padrao != null && padrao.length() > 0) {
+            int[] arrayFalha = gerarArrayDeFalha(padrao);
+            int indiceTexto = 0;
+            int indicePadrao = 0;
+            int contadorComparacoes = 0;
+            while (indiceTexto < texto.length()) {
+                contadorComparacoes++;
+                if (texto.charAt(indiceTexto) == padrao.charAt(indicePadrao)) {
+                    if (indicePadrao == padrao.length() - 1) {
+                        return indiceTexto - indicePadrao;
+                    } else {
+                        indiceTexto = indiceTexto + 1;
+                        indicePadrao = indicePadrao + 1;
+                    }
+                } else if (indicePadrao > 0) {
+                    indicePadrao = arrayFalha[indicePadrao - 1];
                 } else {
                     indiceTexto = indiceTexto + 1;
-                    indicePadrao = indicePadrao + 1;
                 }
-            } else if (indicePadrao > 0) {
-                indicePadrao = arrayFalha[indicePadrao - 1];
-            } else {
-                indiceTexto = indiceTexto + 1;
             }
         }
         return -1;
@@ -40,21 +47,19 @@ public class AlgoritmoKMP {
 
     private static int[] gerarArrayDeFalha(String padrao) {
         int[] arrayFalha = new int[padrao.length()];
-        if (padrao.length() > 0) {
-            arrayFalha[0] = 0;
-            int i = 1;
-            int j = 0;
-            while (i < padrao.length()) {
-                if (padrao.charAt(i) == padrao.charAt(j)) {
-                    arrayFalha[i] = j + 1;
-                    i = i + 1;
-                    j = j + 1;
-                } else if (j > 0) {
-                    j = arrayFalha[j - 1];
-                } else {
-                    arrayFalha[i] = 0;
-                    i = i + 1;
-                }
+        arrayFalha[0] = 0;
+        int indiceArrayFalha = 1;
+        int indicePadrao = 0;
+        while (indiceArrayFalha < padrao.length()) {
+            if (padrao.charAt(indiceArrayFalha) == padrao.charAt(indicePadrao)) {
+                arrayFalha[indiceArrayFalha] = indicePadrao + 1;
+                indiceArrayFalha = indiceArrayFalha + 1;
+                indicePadrao = indicePadrao + 1;
+            } else if (indicePadrao > 0) {
+                indicePadrao = arrayFalha[indicePadrao - 1];
+            } else {
+                arrayFalha[indiceArrayFalha] = 0;
+                indiceArrayFalha = indiceArrayFalha + 1;
             }
         }
         System.out.println("Array de falha para padrão '" + padrao + "': "
